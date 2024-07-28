6fda4ea04c18421d6e0a7d056b836f05bcf8e3f6
RocktimRajkumar <rajkumar.rocktim@gmail.com>
2024-07-28 13:26:33 +0530

    Support different delimiters

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -27,5 +27,10 @@ RSpec.describe StringCalculator do
       calculator = StringCalculator.new
       expect(calculator.add("1\n2,3")).to eq(6)
     end
+
+    it 'returns the sum of numbers with a custom delimiter' do
+      calculator = StringCalculator.new
+      expect(calculator.add("//;\n1;2")).to eq(3)
+    end
   end
 end
